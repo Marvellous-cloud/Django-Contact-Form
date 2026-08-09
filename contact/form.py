cbc4373f03eda24f3417d139c601fba8ec9a288e
Marvellous-cloud <marvelfemzy@gmail.com>
2026-08-09 14:12:35 +0100

    Add Django form validation

--- contact/form.py
+++ contact/form.py
@@ -4,3 +4,19 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+
+        if len(name) < 3:
+            raise forms.ValidationError("Name must be at least 3 characters.")
+
+        return name
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+
+        if len(message) < 10:
+            raise forms.ValidationError("Message must be at least 10 characters.")
+
+        return message
